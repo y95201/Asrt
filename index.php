6ab1fc9f2ee2ab02feff20ce24644d44e3daf39d
y9520 <957612196@qq.com>
2023-09-10 23:20:00 +0800

    修改提交

--- index.php
+++ index.php
@@ -2,11 +2,37 @@
 /*
  * @Author: y95201 957612196@qq.com
  * @Date: 2023-09-08 11:15:58
- * @LastEditors: y95201 957612196@qq.com
- * @LastEditTime: 2023-09-08 12:36:55
+ * @LastEditors: Y95201
+ * @LastEditTime: 2023-09-10 23:01:31
  * @FilePath: /dome/index.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 require "vendor/autoload.php";
-$Test = new Y9520\Asrt\Asrt();
-echo $Test->index();
+
+
+$config = [
+    // 'default' => [
+    //     // 'yunpian',
+    //      'aliyun'
+    // ],
+    'aliyun' => [
+        'access_key_id' => '',
+        'access_key_secret' => '',
+        'sign_name' => '',
+    ],
+    // 'tenxun' => [
+    //     'sdk_app_id' => '', // 短信应用的 SDK APP ID
+    //     'secret_id' => '', // SECRET ID
+    //     'secret_key' => '', // SECRET KEY
+    //     'sign_name' => '腾讯CoDesign', // 短信签名
+    // ],
+    // 'baidu' => [
+    //     'ak' => '',
+    //     'sk' => '',
+    //     'invoke_id' => '',
+    //     'domain' => '',
+    // ],
+];
+
+$Test = new Y9520\Asrt\Asrt($config);
+echo $Test->CreateRecTask();
--- src/Asrt.php
+++ src/Asrt.php
@@ -2,28 +2,34 @@
 /*
  * @Author: y95201 957612196@qq.com
  * @Date: 2023-09-08 11:30:07
- * @LastEditors: y95201 957612196@qq.com
- * @LastEditTime: 2023-09-08 12:54:44
+ * @LastEditors: Y95201
+ * @LastEditTime: 2023-09-10 23:18:10
  * @FilePath: /dome/src/Asrt.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 namespace Y9520\Asrt;
 
 use Pimple\Container;
+use Y9520\Asrt\Support\Config;
+use Y9520\Asrt\Gateways\Gateway;
 class Asrt extends Container
 {
-    public function index()
-    {
-        echo "This is my first composer package";
-    }
-    // public function __get($name)
-    // {
-    //     return $this->offsetGet($name);
-    // }
 
-    // public function __set($name, $value)
-    // {
-    //     $this->offsetSet($name, $value);
-    // }
+    protected $config;
+    public function __construct(array $config)
+    {
+        $this->config = new Config($config);
+    }
+
+    public function CreateRecTask()
+    {
+        // $gateways = $this->config->getName();
+        // $gateways = $this->config->get('default', []);
+        print_r(\strtolower(str_replace([__NAMESPACE__.'\\', 'Gateway'], '', \get_class($this))));die;
+        // foreach($this->config as $key => $value){
+        //     print_r($value->get($key));die;
+        // }
+    }
+    
   
 }
